a89da5e08de8d184dc9ba1014debf99687e6b1bc
Yiran Zhang <zhang.yiran@northeastern.edu>
2022-12-01 15:29:49 -0800

    8

--- exercises/exercise8/Main.java
+++ exercises/exercise8/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         parse p=new parse();
         p.parseXml();
+        p.parseJson();
+        p.addJsonElement();
+        p.parseJson();
+        p.addXmlElement();
+        p.parseXml();
     }
 }
--- exercises/exercise8/parse.java
+++ exercises/exercise8/parse.java
@@ -1,17 +1,29 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class parse {
-    public void parseXml() throws Exception {
+    Document document;
+    JSONObject obj;
+
+    public parse() throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document=builder.parse("./bookshelf.xml");
+        document=builder.parse("./bookshelf.xml");
+        File file=new File("bookshelf.json");
+        String content = new String(Files.readAllBytes(Paths.get("bookshelf.json")));
+        JSONParser jsonParser=new JSONParser();
+        obj = (JSONObject)jsonParser.parse(content);
+    }
+
+    public void parseXml() throws Exception {
         System.out.println("Root element :" + document.getDocumentElement().getNodeName());
         NodeList bookList = document.getElementsByTagName("Book");
         int bookCount = bookList.getLength();
@@ -21,13 +33,46 @@ public class parse {
             NodeList childNodes = book.getChildNodes();
             for(int j=0; j<childNodes.getLength(); j++){
                 if(childNodes.item(j).getNodeType() == Node.ELEMENT_NODE){
-                    System.out.println(childNodes.item(j).getNodeName()+"---" + childNodes.item(j).getTextContent());
+                    System.out.print(childNodes.item(j).getNodeName()+":" + childNodes.item(j).getTextContent()+"---");
                 }
             }
+            System.out.println();
+        }
+    }
+    public void addXmlElement(){
+        Element e=document.createElement("Book");
+        document.getDocumentElement().appendChild(e);
+        Element e2=document.createElement("title");
+        e2.setTextContent("Harry Potter and Goblet of Firee");
+        e.appendChild(e2);
+
+        Element e3=document.createElement("author");
+        e.appendChild(e3);
+        e3.setTextContent("J.K. Rowling");
+        Element e4=document.createElement("publishedYear");
+        e.appendChild(e4);
+        e4.setTextContent("2000");
+        Element e5=document.createElement("numberOfPages");
+        e.appendChild(e5);
+        e5.setTextContent("381");
+    }
+
+    public void parseJson() throws Exception {
+        //System.out.println(obj.get("book"));
+        JSONArray jsonArray=(JSONArray)obj.get("book");
+        for(int i=0;i<jsonArray.size();i++){
+            System.out.print("The "+(i+1)+"th book:");
+            System.out.println(jsonArray.get(i));
         }
     }
 
-    public void addXmlElement(){
-
+    public void addJsonElement(){
+        JSONArray jsonArray=(JSONArray)obj.get("book");
+        JSONObject newobj=new JSONObject();
+        newobj.put("numberOfPages",381);
+        newobj.put("author","J.K. Rowling");
+        newobj.put("publishedYear",2000);
+        newobj.put("title","Harry Potter and Goblet of Firee");
+        jsonArray.add(newobj);
     }
 }
